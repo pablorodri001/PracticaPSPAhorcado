6880c89b39eef77baa02e1ec12b1ce59425119c6
Pablo Yllanes <yllanes.pablo@gmail.com>
2024-02-12 12:36:25 +0100

    Añadidos comentarios en servidor y cliente

--- src/main/java/Cliente/ClienteAhorcado.java
+++ src/main/java/Cliente/ClienteAhorcado.java
@@ -1,4 +1,20 @@
 package Cliente;
 
 public class ClienteAhorcado {
+
+    //Por hacer:
+    //  1. Pedir nombre al jugador
+    //  2. Enviar nombre al servidor para añadir a la tabla jugadores
+    //  3. Recibir respuesta del servidor: Nº de intentos (longitud de palabra) y mostrar caracteres
+    //          (ejemplo: Palabra: _______ , Intentos restantes: 7)
+    //  4. Pedir letra al jugador
+    //  5. Envíar letra al servidor para comprobar si está en la palabra
+    //  6. Recibir y mostrar respuesta del servidor:
+    //      a. Si la letra está en la palabra mostrarla en su posicion (ejemplo: Palabra: ___R___ , Intentos restantes: 6)
+    //      b. Si la letra no está solo se resta el intento (ejemplo: Palabra: _______ , Intentos restantes: 6)
+    //  7. Repetir hasta adivinar todas las letras o hasta que se agoten los intentos.
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/Servidor/ServidorAhorcado.java
+++ src/main/java/Servidor/ServidorAhorcado.java
@@ -1,6 +1,47 @@
 package Servidor;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServidorAhorcado {
+
+    //Por hacer:
+    //  1. Recibir nombre del jugador. Crear objeto y subirlo a tabla jugadores
+    //  2. Enviar mensaje al cliente: Nº de intentos (longitud de palabra) y mostrar caracteres
+    //          (ejemplo: Palabra: _______ , Intentos restantes: 7)
+    //  3. Recibir letra del cliente y comprobar si está en la palabra
+    //  4. Guardar en tabla información de la jugada
+    //  5. Envíar respuesta al cliente:
+    //      a. Si la letra está en la palabra mostrarla en su posicion (ejemplo: Palabra: ___R___ , Intentos restantes: 6)
+    //      b. Si la letra no está solo se resta el intento (ejemplo: Palabra: _______ , Intentos restantes: 6)
+    //  5. Repetir hasta adivinar todas las letras o hasta que se agoten los intentos.
+
+    public static void main(String[] args) {
+        int puerto = 4500;
+        //Nº aleatorio 1-100. Hay que ajustarlo al número de palabras que tengamos:
+        int idPalabra = (int) (Math.random() * 100) + 1;
+
+        try {
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            Socket cliente = null;
+            System.out.println("Esperando al cliente..........");
+            cliente = servidor.accept();
+
+            DataInputStream flujoEntrada = new DataInputStream(cliente.getInputStream());
+            ObjectOutputStream flujoSalida = new ObjectOutputStream(cliente.getOutputStream());
+
+            while(true) {
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
